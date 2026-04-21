7650df4251da0806c7b87ee28a229a22480d4493
azamatkhu <azamat.khu@educa.jcyl.es>
2026-04-21 13:19:40 +0200

    Bulk upload

--- src/MainSalarioPromedio.java
+++ src/MainSalarioPromedio.java
@@ -1,15 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.sql.*;
+import java.util.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class MainSalarioPromedio {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:oracle:thin:@localhost:1521:xe",
+                    "RIBERA",
+                    "ribera"
+            );
+
+            System.out.println("Conectado!");
+
+
+            String sql = "SELECT AVG(SALARIO) FROM EMPLEADO";
+            Statement stam = connection.createStatement();
+
+            ResultSet resultSet = stam.executeQuery(sql);
+
+            if (resultSet.next()) {
+                System.out.println("El salario promedio es: " + resultSet.getDouble(1));
+            }
+
+            resultSet.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 }
